6cc0d519ec38498db50fa073fb1a933b5987dcb2
d3stroya <anabel.comino@gmail.com>
2025-10-03 12:43:27 +0200

    Apuntes Tema 3 - Condicionales
    * If
    * If else
    * switch case

---
+++ Tema3/src/tema3/EstructurasDeControl.java
@@ -0,0 +1,86 @@
+package tema3;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author d3stroya
+ */
+public class EstructurasDeControl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 1. IF
+        boolean eresPalestino = true;
+        int edad = 10;
+        
+//        if(eresPalestino == true) {
+//            System.out.println("Netanyahu te lanza un misil");
+//        }
+
+//        if(eresPalestino && edad < 16) {
+//            System.out.println("Refugiarte en un hospital no te va a salvar");
+//        }
+        
+        
+        // 2A. IF-ELSE
+//        if(eresPalestino) {
+//            System.out.println("Netanyahu te lanza un misil");
+//        } else {
+//            System.out.println("Netanyahu todavía no va a por ti");
+//        }        
+        
+        // 2B. IF-ELSE-IF
+//        if(eresPalestino) {
+//            System.out.println("Netanyahu te lanza un misil");
+//        } else if(edad < 10) {
+//            System.out.println("Tu escuela aún está en pie");
+//        } else {
+//            System.out.println("Netanyahu todavía no va a por ti");            
+//        }        
+        
+        
+        // 3. SWITCH-CASE
+        
+        // Declaramos las variables
+//        int opcion;
+//        Scanner entrada = new Scanner(System.in);
+//
+//        // Mostramos el menú
+//        System.out.println("Elija una opción:");
+//        System.out.println("1. Mirar a otro lado");
+//        System.out.println("2. Protestar pacíficamente");
+//        System.out.println("3. Entrar en guerra");
+//        System.out.println("4. Aportar ayuda humanitaria");
+//        System.out.println("5. Salir");
+//
+//        // Leemos la opción elegida por el usuario 
+//        // y la guardamos en la variable opcion
+//        opcion = entrada.nextInt();
+//
+//        // Ejecutamos el switch-case
+//        switch(opcion) {
+//            case 1: 
+//                System.out.println("Has elegido mirar a otro lado");
+//                break;
+//            case 2:
+//                System.out.println("Has elegido protestar pacíficamente");
+//                break;
+//            case 3:
+//                System.out.println("Has elegido entrar en guerra");
+//                break;
+//            case 4: 
+//                System.out.println("Has elegido aportar ayuda humanitaria");
+//                break;
+//            case 5:
+//                System.out.println("Gracias por usar el programa");
+//                break;
+//            default:
+//                System.out.println("Opción no válida. Selecciona un valor entre 1 y 5.");
+//        }
+    }    
+
+}
